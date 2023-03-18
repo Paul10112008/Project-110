032d6bd93f0b146e79c2d3c48e586cca28a1b495
Paul10112008 <90984170+Paul10112008@users.noreply.github.com>
2023-03-18 19:51:03 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+//https://teachablemachine.withgoogle.com/models/j0r1zPUpe/
+Webcam.set({
+width:350,
+height:300,
+image_format : 'png',
+png_quality: 80
+});
+
+camera = document.getElementById("camera");
